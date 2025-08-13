d8e94ab0f687d028cafd0b970f4e189f77838aa5
Atom <atomcraft.4u@web.de>
2025-08-13 22:19:50 +0200

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Annahme: Du verwendest ein HTML-Element mit der ID "home-btn"
 document.getElementById('home-btn').addEventListener('click', function() {
-    window.location.href = '/bundestag.html'; // Passe den Pfad ggf. an
+    window.location.href = '/bundestag/'; // Passe den Pfad ggf. an
 });
