2ef2f6e04a7ead569473b3fe8892e2224ff6fa26
Atom <atomcraft.4u@web.de>
2025-08-16 02:44:15 +0200

    fix

--- bundestag/login/mdbReg.js
+++ bundestag/login/mdbReg.js
@@ -2,20 +2,15 @@ document.getElementById('bt-btn').addEventListener('click', function() {
     window.location.href = '/bundestag/'; // Passe den Pfad ggf. an
 });
 document.getElementById('mdbReg-btn').addEventListener('click', function() {
-    window.location.href = '/bundestag/login/mdbReg'; // Passe den Pfad ggf. an
+    window.location.href = '/bundestag/login/mdbReg.html'; // Passe den Pfad ggf. an
 });
 document.getElementById('commit-btn').addEventListener('click', commit());
 
-function commit() {
+async function commit() {
     const firstName = document.getElementById('firstName').value;
     // Hier kannst du den Code hinzufügen, um die Daten an den Server zu senden
     await pool.query('INSERT INTO mdb (name_) VALUES '+ $(name-txt.txt) +')');
     await pool.end();
     console.log(`Committed`);
     window.location.href = '/bundestag/';
-);
-
-await pool.end();
-    console.log(`Commit to server: ${name-txt.txt}`);
-    
-});
+}
